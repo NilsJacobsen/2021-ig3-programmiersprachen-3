22e84e8b47eb12dbe0bbf36d2f8ed1e7dec1e9da
Nils Jacobsen <nils.jacobsen98@gmail.com>
2020-10-29 19:07:48 +0100

    added round slider

--- coffee_maker/js/main.js
+++ coffee_maker/js/main.js
@@ -1,4 +1,7 @@
-//slider 
+//slider
+var sliderTop;
+var sliderX;
+
 
 $(document).ready(function(){
     $('.slider-container').slick({
@@ -8,6 +11,25 @@ $(document).ready(function(){
         centerMode: true,
         variableWidth: true,
         dots: false,
-        arrows: false
+        arrows: false,
+        speed: 150
     });
+    $('.slider').on('mousedown', function(){
+        console.log("bin da");
+    });
+    $('.slider-container').on('afterChange', function(event, slick, currentSlide, nextSlide){
+        console.log(event.currentTarget);
+    });
+    sliderTop = $('.slick-center').offset().top;
+    sliderX = $('.slick-center').offset().left;
 });
+
+setInterval(function(){ 
+    var slides = $('.slider-elem')
+    for(var i  = 0; i < slides.length; i++){
+        let x = sliderX - $(slides[i]).offset().left;
+        let newTop = Math.pow(Math.abs(x),2)/1500 + sliderTop;
+        $(slides[i]).offset({ top: newTop});
+    }
+},20);
+
