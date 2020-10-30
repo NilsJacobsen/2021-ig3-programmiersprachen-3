8c8d5d09546f36c674beff937bd4ceda5e88e4b4
Nils Jacobsen <nils.jacobsen98@gmail.com>
2020-10-30 13:57:16 +0100

    slider circle

--- coffee_maker/js/main.js
+++ coffee_maker/js/main.js
@@ -2,7 +2,6 @@
 var sliderTop;
 var sliderX;
 
-
 $(document).ready(function(){
     $('.slider-container').slick({
         infinite: true,
@@ -37,13 +36,7 @@ setInterval(function(){
     var slides = $('.slider-child');
     var parents = $('.slider-elem');
     for(var i  = 0; i < slides.length; i++){
-        let x = sliderX - $(slides[i]).offset().left;
-        var offset = Math.abs(x)/15;
-        $(slides[i]).css("width", 200 - offset);
-        $(slides[i]).css("height", 200 - offset);
-        let sliderLeft = $(parents[i]).offset().left;
-        let newTop = Math.pow(Math.abs(x),2)/1700 + sliderTop + 20;
-        $(slides[i]).offset({ top: newTop + offset/2, left: sliderLeft + offset/2});
+        
     }
 },50);
 
