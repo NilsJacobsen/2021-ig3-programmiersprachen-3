8bcecc1bf3a10640e4957537cfd05a3a011dad2d
carolinebunk <73487202+carolinebunk@users.noreply.github.com>
2020-10-31 13:14:58 +0100

    ingredients and selected

---
+++ coffee_maker/js/ingredients.js
@@ -0,0 +1,91 @@
+const ingredients = {
+  size: {
+    cups: {
+      name: 'cups',
+      url: '../assets/cups.png',
+      default: false
+    },
+    cupm: {
+      name: 'cupm',
+      url: '../assets/cupm.png',
+      default: true
+    },
+    cupl: {
+      name: 'cupl',
+      url: '..assets/cupl.png',
+      default: false
+    }
+  },
+
+  beans: {
+    beans1: {
+      name: 'beans1',
+      url: '../assets/beans1.png',
+      default: false
+    },
+    beans2: {
+      name: 'beans2',
+      url: '../assets/beans2.png',
+      default: true
+    },
+    beans3: {
+      name: 'beans3',
+      url: '../assets/beans3.png',
+      default: false
+    }
+  },
+
+  temperature: {
+    hot: {
+      name: 'hot',
+      url: '../assets/hot.png',
+      default: true
+    },
+    cold: {
+      name: 'cold',
+      url: '../assets/cold.png',
+      default: false
+    }
+  },
+
+  milk: {
+    soy: {
+      name: 'soy',
+      url: '../assets/soy.png',
+      default: false
+    },
+    oat: {
+      name: 'oat',
+      url: '../assets/oat.png',
+      default: false
+    },
+    cow: {
+      name: 'cow',
+      url: '../assets/cow.png',
+      default: true
+    },
+    almond: {
+      name: 'almond',
+      url: '../assets/almond.png',
+      default: false
+    },
+  },
+
+  sugar: {
+    sugar1: {
+      name: 'sugar1',
+      url: '../assets/sugar1.png',
+      default: false
+    },
+    sugar2: {
+      name: 'sugar2',
+      url: '../assets/sugar2.png',
+      default: true
+    },
+    sugar3: {
+      name: 'sugar3',
+      url: '..assets/sugar3.png',
+      default: false
+    }
+  }
+};
---
+++ coffee_maker/js/selected.js
@@ -0,0 +1,36 @@
+const selected = {
+  size: {
+    cupm: {
+      name: 'cupm',
+      url: '../assets/cupm.png'
+    }
+  },
+
+  beans: {
+    beans2: {
+      name: 'beans2',
+      url: '../assets/beans2.png',
+    }
+  },
+
+  temperature: {
+    hot: {
+      name: 'hot',
+      url: '../assets/hot.png'
+    }
+  },
+
+  milk: {
+    cow: {
+      name: 'cow',
+      url: '../assets/cow.png'
+    }
+  },
+
+  sugar: {
+    sugar2: {
+      name: 'sugar2',
+      url: '../assets/sugar2.png'
+    }
+  }
+};
